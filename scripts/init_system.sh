0638d8610deeeae37dda36662a55862db2c7d26a
jon <jon.li@gllue.com>
2018-04-28 10:58:57 +0800

    :star: [#] init_system install docker use tsinghua mirror

--- scripts/init_system.sh
+++ scripts/init_system.sh
@@ -138,7 +138,11 @@ install_docker () {
     if docker -h >> /dev/null; then
         echo docker installed, skip ......
     else
-        curl -sSL https://get.daocloud.io/docker | sudo sh
+        sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update
+        sudo apt-get install docker-ce
         sudo usermod -aG docker $USER
     fi
 }
