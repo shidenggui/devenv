074afee3d18f41170b33d736b47ebe7795c57ecb
jon <jon.li@gllue.com>
2018-05-10 16:19:32 +0800

    :bug: [#] init_system apt-get install add -y params

--- scripts/init_system.sh
+++ scripts/init_system.sh
@@ -138,11 +138,11 @@ install_docker () {
     if docker -h >> /dev/null; then
         echo docker installed, skip ......
     else
-        sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+        sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
         sudo apt-get update
-        sudo apt-get install docker-ce
+        sudo apt-get install -y docker-ce
         sudo usermod -aG docker $USER
     fi
 }
